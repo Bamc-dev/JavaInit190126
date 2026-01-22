cb1633593efabe27929e099f69a70c2844cf224f
Bamc <brannigan.pro@gmail.com>
2026-01-22 11:55:47 +0100

    added example for collections

---
+++ src/main/java/fr/dawan/collections/Application.java
@@ -0,0 +1,12 @@
+package fr.dawan.collections;
+
+public class Application {
+    public static void main(String[] args) {
+        Utilisateur user1 = new Utilisateur("Alex", "Andry", 38);
+        Utilisateur user2 = new Utilisateur("Luc", "Harne", 55);
+        Utilisateur user3 = new Utilisateur("Melanie", "Mal", 24);
+        Utilisateur user4 = new Utilisateur("Didier", "Tétique", 18);
+        Utilisateur user5 = new Utilisateur("Mickey", "Mouse", 24);
+        Utilisateur user6 = new Utilisateur("Melanie", "Mal", 24);
+    }
+}
